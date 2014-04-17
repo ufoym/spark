026caac450ab62fc368e3e283a3939e621a2ece2
Ming <a@ufoym.com>
2014-04-17 18:11:20 +0800

    Merge student info

--- parse.py
+++ parse.py
@@ -4,9 +4,20 @@ import json
 fn_in = 'in.csv'
 fn_json= 'out.json'
 
-select = [12, 3, 5, 4, 6, 8, 9, 2, 11, 15]
+select = [12, 3, 5, 8, 9, 2, 11, 15]
 cols = []
 data = []
+
+def add_term(terms, s):
+    if s == 5:
+        name = terms[s].strip()
+        school = terms[4].strip()
+        grade = terms[6].strip()
+        terms.append(name + '<br/>' + school + '<br/>' + grade)
+    else:
+        term = terms[s].strip()
+        terms.append(term)
+
 with open(fn_in, 'r') as f:
     for i, line in enumerate(f):
         line = line.decode('gbk').encode('utf-8')
@@ -16,7 +27,8 @@ with open(fn_in, 'r') as f:
             cols = terms
         else:
             terms = line.split(',')
-            terms = [terms[s].strip() for s in select]
+            for s in select:
+                add_term(terms, s)
             if ''.join(terms) != '':
                 entry = {u'操作': '', u'编号': i}
                 for col, term in zip(cols, terms):
@@ -30,19 +42,12 @@ with open(fn_json, 'w') as f:
             'label':col,
             'datatype':'string',
             'editable':True
-        } for col in cols],
+        } for col in [u'编号'] + cols + [u'操作']],
         'data': data
     }
-    obj['metadata'].insert(0, {
-            'name':u'编号',
-            'label':u'编号',
-            'datatype':'integer',
-            'editable':False
-        })
-    obj['metadata'].append({
-            'name':u'操作',
-            'label':u'操作',
-            'datatype':'html',
-            'editable':False
-        })
+    obj['metadata'][0]['datatype'] = 'integer'
+    obj['metadata'][0]['editable'] = False
+    obj['metadata'][-1]['datatype'] = 'html'
+    obj['metadata'][-1]['editable'] = False
+    obj['metadata'][3]['datatype'] = 'html'
     json.dump(obj, f)
